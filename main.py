4905fa12da459ee6965f83a33ac64f328ba1e9da
Joshua Barwell <94557836+jtbarwell@users.noreply.github.com>
2025-07-15 10:23:51 -0400

    wrote basic main func

---
+++ main.py
@@ -0,0 +1,14 @@
+import cloudscraper
+import pandas as pd
+from bs4 import BeautifulSoup
+from scanTables import *
+
+
+def main():
+    url = "https://www.baseball-reference.com/teams/TOR/2025.shtml"
+    # print(getBattingDF(url))
+    # print(getPitchingDF(url))
+    # print(getFieldingDF(url))
+
+if __name__ == "__main__":
+    main()
