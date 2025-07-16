9889e6130c3bc4d7e4e62615c1d72bc532abebd2
Joshua Barwell <jtbarwell@gmail.com>
2025-07-16 12:26:25 -0400

    added list for team codes

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from scanTables import getBattingDF
 
 def main():
     urlFrame = "https://www.baseball-reference.com/teams/"
+    teamCodes = ['PHI', 'NYM', 'MIA', 'ATL', 'WSN', 'CHC', 'MIL', 'STL', 'CIN', 'PIT', 'LAD', 'SDP', 'SFG', 'ARI', 'COL', 'TOR', 'NYY', 'BOS', 'TBR', 'BAL', 'DET', 'MIN', 'CLE', 'KCR', 'CHW', 'HOU', 'SEA', 'TEX', 'LAA', 'ATH']
     print(getInsertBattingString(urlFrame, 'TOR', '2025'))
     # print(getBattingDF("https://www.baseball-reference.com/teams/TOR/2025.shtml"))
 
