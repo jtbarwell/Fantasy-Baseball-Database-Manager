bbf884ce712c4df0a07cb1715d3769e884029cc6
Joshua Barwell <jtbarwell@gmail.com>
2025-07-16 10:19:05 -0400

    added createTables.sql, and wrote create script for BattingStats table

---
+++ createTables.sql
@@ -0,0 +1,37 @@
+drop table if exists BattingStats;
+
+
+
+
+create table BattingStats (
+    Player  nvarchar(150),
+    Age     int,
+    Pos     nvarchar(15),
+    WAR     numeric(2, 1),
+    G       int,
+    PA      int,
+    AB      int,
+    R       int,
+    H       int,
+    2B      int,
+    3B      int,
+    HR      int,
+    RBI     int,
+    SB      int,
+    CS      int,
+    BB      int,
+    SO      int,
+    BA      numeric(4, 3),
+    OBP     numeric(4, 3),
+    SLG     numeric(4, 3),
+    OPS     numeric(4, 3),
+    OPS+    int,
+    rOBA    numeric(4, 3),
+    Rbat+   int,
+    TB      int,
+    GIDP    int,
+    HBP     int,
+    SH      int,
+    SF      int,
+    IBB     int,
+);
