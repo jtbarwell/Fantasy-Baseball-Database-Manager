a331eb3036f654b857798ad8f7dea2834670358c
Joshua Barwell <jtbarwell@gmail.com>
2025-07-16 11:41:29 -0400

    calling different func for full team history

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
 import cloudscraper
 import pandas as pd
 from bs4 import BeautifulSoup
+from insertBatting import getInsertBattingString
+
 from scanTables import getBattingDF
 
-
 def main():
-    url = "https://www.baseball-reference.com/teams/TOR/2025.shtml"
-    battingDF = getBattingDF(url)
-    quote_columns=['Player', 'Pos']
-    for index, row in battingDF.iterrows():
-        values = []
-        for col in battingDF.columns:
-            val = row[col]
-            if col in quote_columns:
-                val = f"'{val}'"
-            else:
-                if val == '':
-                    val = 0
-            values.append(str(val))
-        print(", ".join(values))
-    
+    urlFrame = "https://www.baseball-reference.com/teams/"
+    # print(getInsertBattingString(urlFrame, 'ARI', '2025'))
+    print(getBattingDF("https://www.baseball-reference.com/teams/TOR/2025.shtml"))
 
-    # print(getPitchingDF(url))
-    # print(getFieldingDF(url))
 
 if __name__ == "__main__":
     main()
