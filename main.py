231ac672d3bd1878da6bd4b69fadbbe217495bac
Joshua Barwell <jtbarwell@gmail.com>
2025-07-16 10:18:35 -0400

    Added CSV support loop to main

--- main.py
+++ main.py
@@ -7,9 +7,19 @@ from scanTables import getBattingDF
 def main():
     url = "https://www.baseball-reference.com/teams/TOR/2025.shtml"
     battingDF = getBattingDF(url)
-    # print(battingDF)
-    # for r in battingDF.iterrows():
-    #     print(r)
+    quote_columns=['Player', 'Pos']
+    for index, row in battingDF.iterrows():
+        values = []
+        for col in battingDF.columns:
+            val = row[col]
+            if col in quote_columns:
+                val = f"'{val}'"
+            else:
+                if val == '':
+                    val = 0
+            values.append(str(val))
+        print(", ".join(values))
+    
 
     # print(getPitchingDF(url))
     # print(getFieldingDF(url))
