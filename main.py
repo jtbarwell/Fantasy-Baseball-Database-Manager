fdf40a0ffd6a7504c79684dad5964dc1fb1af290
Joshua Barwell <jtbarwell@gmail.com>
2025-08-13 12:22:57 -0400

    reset the main function pathway

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ def main():
     teamCodes = ['PHI', 'NYM', 'MIA', 'ATL', 'WSN', 'CHC', 'MIL', 'STL', 'CIN', 'PIT', 'LAD', 'SDP', 'SFG', 'ARI', 'COL', 'TOR', 'NYY', 'BOS', 'TBR', 'BAL', 'DET', 'MIN', 'CLE', 'KCR', 'CHW', 'HOU', 'SEA', 'TEX', 'LAA', 'ATH']
     # teamCodes = ['TOR']
     for team in teamCodes:
-        insertBatting(urlFrame, team, '2024')
+        # insertBatting(urlFrame, team, '2025') # done
+        # insertPitching(urlFrame, team, '2025')
+        # insertFielding(urlFrame, team, '2025')
+        pass
 
 if __name__ == "__main__":
     main()
