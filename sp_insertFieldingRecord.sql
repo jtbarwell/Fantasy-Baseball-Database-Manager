043dbfb8ffabc4ae96ae27b9d28f6bfbec3a9202
Joshua Barwell <jtbarwell@gmail.com>
2025-08-13 12:34:06 -0400

    Fielding Stats Insert Stored procedure updates

--- sp_insertFieldingRecord.sql
+++ sp_insertFieldingRecord.sql
@@ -1,43 +1,73 @@
 -- =============================================
--- Author:      <Author, sysname, Your Name>
--- Create Date: <Create Date,,>
--- Description: <Description,,>
+-- Author:      <Joshua Barwell, DESKTOP-IEKNRR8\SQLEXPRESS | baseball>
+-- Create Date: <2025-08-07>
+-- Description: <Stored procedure to insert data into the FieldingStats table via JSON string>
 -- =============================================
-CREATE PROCEDURE sp_insertFieldingRecord
-	@Year    	int,
-	@Team    	nvarchar(30),
-	@Player		nvarchar(150),
-	@Age		int,
-	@G			int,
-	@GS			int,
-	@CG			int,
-	@Inn		float,
-	@Ch			int,
-	@PO			int,
-	@A			int,
-	@E			int,
-	@DP			int,
-	@Fld_pct    decimal(4, 3),
-	@Rtot		int,
-	@Rtot_yr	int,
-	@RF_9		float,
-	@lgRF9		float,
-	@PB			int,
-	@WP			int,
-	@SB			int,
-	@CS			int,
-	@CS_pct		float,
-	@Pick		int,
-	@Pos		nvarchar(30)
+use [baseball];
+GO
+
+create or alter PROCEDURE sp_insertFieldingRecord
+	@fieldingStatJSON nvarchar(MAX)
 AS  
 BEGIN
-    -- SET NOCOUNT ON added to prevent extra result sets from
-    -- interfering with SELECT statements.
     SET NOCOUNT ON;
 
-    insert into baseball.dbo.FieldingStats 
-        (Year, Team, Player, Age, G, GS, CG, Inn, Ch, PO, A, E, DP, [Fld%], Rtot, [Rtot/yr], [RF/9], lgRF9, PB, WP, SB, CS, [CS%], Pick, Pos)
-    values
-        (@Year, @Team, @Player, @Age, @G, @GS, @CG, @Inn, @Ch, @PO, @A, @E, @DP, @Fld_pct, @Rtot, @Rtot_yr, @RF_9, @lgRF9, @PB, @WP, @SB, @CS, @CS_pct, @Pick, @Pos);
-    
-END
+
+    INSERT INTO FieldingStats 
+        ([Year], Team, Player, Age, Pos, G, GS, CG, Inn, Ch, PO, A, E, DP, [Fld%], Rtot, [Rtot/yr], [RF/9], lgRF9, PB, WP, SB, CS, [CS%], Pick))
+    SELECT
+        TRY_CAST([Year]  	AS int),
+        Team,
+        Player,
+        TRY_CAST(Age 	 	AS int),
+        Pos,
+		TRY_CAST(G			AS int),
+		TRY_CAST(GS			AS int),
+		TRY_CAST(CG			AS int),
+		TRY_CAST(Inn		AS float),
+		TRY_CAST(Ch			AS int),
+		TRY_CAST(PO			AS int),
+		TRY_CAST(A			AS int),
+		TRY_CAST(E			AS int),
+		TRY_CAST(DP			AS int),
+		TRY_CAST([Fld%]		AS decimal(4, 3)),
+		TRY_CAST(Rtot		AS int),
+		TRY_CAST([Rtot/yr]	AS int),
+		TRY_CAST([RF/9]		AS float),
+		TRY_CAST(lgRF9		AS float),
+		TRY_CAST(PB			AS int),
+		TRY_CAST(WP			AS int),
+		TRY_CAST(SB			AS int),
+		TRY_CAST(CS			AS int),
+		TRY_CAST([CS%]		AS float),
+		TRY_CAST(Pick		AS int)
+    FROM OPENJSON(@fieldingStatJSON)
+    WITH (
+        [Year]    nvarchar(10)  '$.Year',
+        Team      nvarchar(MAX) '$.Team',
+        Player    nvarchar(MAX) '$.Player',
+        Age       nvarchar(10)  '$.Age',
+        Pos       nvarchar(MAX) '$.Pos',
+		G		  nvarchar(10)  '$.G',
+		GS		  nvarchar(10)  '$.GS',
+		CG		  nvarchar(10)  '$.CG',
+		Inn		  nvarchar(10)  '$.Inn',
+		Ch		  nvarchar(10)  '$.Ch',
+		PO		  nvarchar(10)  '$.PO',
+		A		  nvarchar(10)  '$.A',
+		E		  nvarchar(10)  '$.E',
+		DP		  nvarchar(10)  '$.DP',
+		[Fld%]	  nvarchar(10)  '$."[Fld%]"',
+		Rtot	  nvarchar(10)  '$.Rtot',
+		[Rtot/yr] nvarchar(10)  '$."[Rtot/yr]"',
+		[RF/9]	  nvarchar(10)  '$."[RF/9]"',
+		lgRF9	  nvarchar(10)  '$.lgRF9',
+		PB		  nvarchar(10)  '$.PB',
+		WP		  nvarchar(10)  '$.WP',
+		SB		  nvarchar(10)  '$.SB',
+		CS		  nvarchar(10)  '$.CS',
+		[CS%]	  nvarchar(10)  '$."[CS%]"',
+		Pick	  nvarchar(10)  '$.Pick'
+    );
+END;
+GO
